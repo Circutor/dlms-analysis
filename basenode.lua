527cf94ec55e91666a2956fe008090d43daaee33
Joan Duran <jduran@circutor.com>
2024-07-16 13:04:09 +0200

    Improved Base Node

--- basenode.lua
+++ basenode.lua
@@ -20,11 +20,16 @@ local BNType = {
 	[4] = "Delete meters",
 	[5] = "Enable auto close",
 	[6] = "Disable auto close",
+	[7] = "Unregister meters",
+	[8] = "Phase result",
+	[9] = "Phase request",
 }
 
 local Type = ProtoField.uint8("bn.type","Type",base.HEX,BNType)
+local DeviceID = ProtoField.uint16("bn.deviceID","DeviceID",base.HEX)
+local EUI48 = ProtoField.string("bn.eui48","EUI48")
 
-bn_proto.fields = {Type}
+bn_proto.fields = {Type, DeviceID, EUI48}
 
 -- Create a simple dissection function
 function bn_proto.dissector(buffer, pinfo, tree)
@@ -38,5 +43,18 @@ function bn_proto.dissector(buffer, pinfo, tree)
     
     local bnType = buffer(0,1):uint()
     
-    pinfo.cols['info'] = BNType[bnType]
+    if bnType == 8 or bnType == 9 then
+        local instance = buffer(1,6)
+        local eui48 = string.format("%02x:%02x:%02x:%02x:%02x:%02x",instance(0,1):uint(),instance(1,1):uint(),instance(2,1):uint(),instance(3,1):uint(),instance(4,1):uint(),instance(5,1):uint())
+        
+        t_bn:add(EUI48, eui48)
+        
+        pinfo.cols['info'] = BNType[bnType].." EUI48="..eui48
+    else
+        pinfo.cols['info'] = BNType[bnType]
+    end
+    
+    if bnType == 1 or bnType == 2 or bnType == 4 or bnType == 5 or bnType == 6 or bnType == 7 then
+    	t_prime:add(LNID, buffer(1,2))
+    end
 end
